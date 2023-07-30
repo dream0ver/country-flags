25e05161306a50c81bb19f053d55fe4957d2b2aa
Chaithanya <110430014+dream0ver@users.noreply.github.com>
2023-07-30 16:41:42 +0530

    Update ThemeContext.tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -9,7 +9,16 @@ export const ThemeContext = React.createContext<ThemeContextType>(INITIAL_STATE)
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = React.useState<ThemeType>("light-theme")
   const toggleTheme = () => {
-    setTheme(prev => (prev == "light-theme" ? "dark-theme" : "light-theme"))
+    setTheme(prev =>{
+      if(prev == "light-theme"){
+        document.body.classList.add("dark-theme")
+        document.body.classList.remove("light-theme")
+      }
+      else{
+        document.body.classList.add("light-theme")
+        document.body.classList.remove("dark-theme")
+      }
+    })
   }
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
